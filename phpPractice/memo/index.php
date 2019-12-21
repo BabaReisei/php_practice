90e8e689363350d69d2775eef4c9b36e2ee2fb4c
BabaReisei <47935167+BabaReisei@users.noreply.github.com>
2019-12-21 19:51:39 +0900

    セクション4-57 追加

--- phpPractice/memo/index.php
+++ phpPractice/memo/index.php
@@ -17,26 +17,26 @@
 
 <main>
 <h2>Practice</h2>
-<pre>
+
 <?php
     try{
         // IPアドレスはMySQLコンテナIDを取得後、docker exec -it xxxx bashでコンテナ内部に入り、のhost-iで確認
         $db = new PDO('mysql:dbname=mydb;host=172.18.0.2;charset=utf8',
         'root', 'pass');
     } catch (PDOException $e) {
-        echo 'DB接続エラー：'. $e->getMessage();
+        echo 'DB接続エラー：'. $e -> getMessage();
     }
     $memos = $db -> query('SELECT * FROM memos ORDER BY id DESC');
 ?>
+
 <article>
     <?php while ($memo = $memos -> fetch()): ?>
-        <p><a href="#"><?php print($memo['memo']); ?></a></p>
+        <p><a href="#"><?php print(mb_substr($memo['memo'], 0, 50)); ?></a></p>
         <time><?php print($memo['created_at']); ?></time>
         <hr>
     <?php endwhile; ?>
 </article>
 
-</pre>
 </main>
 </body>    
 </html>
